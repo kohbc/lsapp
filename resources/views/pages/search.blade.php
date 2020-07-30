faec456fbc6015994de4fddf3f26a382858326c5
kohbc <63275782+kohbc@users.noreply.github.com>
2020-07-30 14:18:28 +1000

    pushing all files onto github

---
+++ resources/views/pages/search.blade.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="initial-scale=1">
+
+        <title>{{config('app.name', 'LSAPP')}}</title>
+
+    </head>
+    <body>
+    
+    <div id="body-frame" style="width:1000px; position: absolute; z-index: 15; left: 50%; margin-left: -500px; border:2px solid black;">
+
+        <div id="top-box" align="center" style="height:30px; border-bottom:1px solid black; padding-top:10px;">
+            <label for="logo">LOGO</label>
+            <input type="text" name="searchFunction" id="searchFunction" placeholder=" Search" />
+            <button type="button" name="searchButton" id="searchButton"  style="margin-right:20px;">Login</button>
+            <a href="home" style="margin-right:20px;">Home</a>
+            <label style="margin-right:20px;">|</label>
+            <a href="about" style="margin-right:20px;">About</a>
+            <label style="margin-right:20px;">|</label>
+            <a href="login"style="margin-right:20px;">Login</a>
+            <label style="margin-right:20px;">|</label>
+            <a href="signup" style="margin-right:20px;">Sign up</a>
+        </div>
+       
+    </div>
+
+
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+//localhost/lsapp/public
+Route::get('/', 'PagesController@index');
+Route::get('/about', 'PagesController@about');
+Route::get('/services', 'PagesController@services');
+Route::resource('posts', 'PostsController');
+Route::get('/homepage1', 'PagesController@homepage1');
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return 'Hello World';
+});
+
+Route::get('/users/{id}/{name}', function($id, $name){
+    return 'This is user '.$name.' with an id of '.$id;
+});
+*/
+
+Auth::routes();
+
+//Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('/dashboard', 'DashboardController@index');
+
+Route::get('/login/google', 'Auth\LoginController@redirectToProvider');
+Route::get('/login/google/callback', 'Auth\LoginController@handleProviderCallback');
