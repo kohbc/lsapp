f269c624baebc0476d547bb7015bb4e9ebbdd620
kohbc <63275782+kohbc@users.noreply.github.com>
2020-10-06 01:46:01 +1100

    kill me, for realz

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,30 +7,32 @@
             <div class="panel panel-default">
                 <div class="panel-body">
                     <h3>Username: {{Auth::user()->name}} E-mail: {{Auth::user()->email}}</h3><br/>
-                    <a href="/quizzes/create" class="btn btn-primary">Create Quiz</a>
-                    <h3>Your Quizzes</h3>
-                    @if(count($quizzes) > 0)
-                        <table class="table table-striped">
-                            <tr>
-                                <th>Title</th>
-                                <th></th>
-                                <th></th>
-                            </tr>
-                            @foreach($quizzes as $quiz)
+                    @if(Auth::user()->level >= 2)
+                        <a href="/quizzes/create" class="btn btn-primary">Create Quiz</a>
+                        <h3>Your Quizzes</h3>
+                        @if(count($quizzes) > 0)
+                            <table class="table table-striped">
                                 <tr>
-                                    <td>{{$quiz->title}}</td>
-                                    <td><a href="/quizzes/{{$quiz->id}}/edit">Edit</a></td>
-                                    <td>
-                                        {!!Form::open(['action' => ['QuizzesController@destroy', $quiz->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
-                                            {{Form::hidden('_method', 'DELETE')}}
-                                            {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
-                                        {!!Form::close()!!}
-                                    </td>
+                                    <th>Title</th>
+                                    <th></th>
+                                    <th></th>
                                 </tr>
-                            @endforeach
-                        </table>
-                    @else
-                        <p>You have no post.</p>
+                                @foreach($quizzes as $quiz)
+                                    <tr>
+                                        <td>{{$quiz->title}}</td>
+                                        <td><a href="/quizzes/{{$quiz->id}}/edit">Edit</a></td>
+                                        <td>
+                                            {!!Form::open(['action' => ['QuizzesController@destroy', $quiz->id], 'method' => 'POST', 'class' => 'pull-right'])!!}
+                                                {{Form::hidden('_method', 'DELETE')}}
+                                                {{Form::submit('Delete', ['class' => 'btn btn-danger'])}}
+                                            {!!Form::close()!!}
+                                        </td>
+                                    </tr>
+                                @endforeach
+                            </table>
+                        @else
+                            <p>You have no post.</p>
+                        @endif
                     @endif
                 </div>
             </div>
