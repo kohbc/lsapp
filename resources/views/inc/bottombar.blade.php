09b4c539179d528e4d725fce6d08b3063dc8cd70
kmt673 <tkm9968@gmail.com>
2020-10-09 13:06:30 +1100

    bottombar 2.0

--- resources/views/inc/bottombar.blade.php
+++ resources/views/inc/bottombar.blade.php
@@ -1,31 +1,38 @@
-<nav class="navbar-bottom">
+<nav class="bar-navigation surface">
     <div class="container">
-        <div class="navbar-header">
-            <a class="{{(Route::currentRouteName() === 'resources.index') ? 'active' : ''}}" href="/resources"><img src="/storage/cover_image/baseline_open_in_new_white_18dp.png" class="center" alt="resources">
-                @if (Route::currentRouteName() === 'resources.index')
-                    <span>Resources</span>
-                @endif
-            </a>
-            <a class="{{(Route::currentRouteName() === 'account.index') ? 'active' : ''}}" href="/account"><img src="/storage/cover_image/baseline_account_circle_white_18dp.png" class="center" alt="account">
-                @if (Route::currentRouteName() === 'account.index')
-                    <span>Account</span>
-                @endif
-            </a>
-            <a class="{{(Route::currentRouteName() === 'activities.index') ? 'active' : ''}}" href="/activities"><img src="/storage/cover_image/baseline_videogame_asset_white_18dp.png" class="center" alt="activities">
-                @if (Route::currentRouteName() === 'activities.index')
-                    <span>Activities</span>
-                @endif
-            </a>
-            <a class="{{(Route::currentRouteName() === 'social.index') ? 'active' : ''}}" href="/social"><img src="/storage/cover_image/baseline_supervisor_account_white_18dp.png" class="center" alt="social">
-                @if (Route::currentRouteName() === 'social.index')
-                    <span>Social</span>
-                @endif
-            </a>
-            <a class="{{(Route::currentRouteName() === 'ranking.index') ? 'active' : ''}}" href="/ranking"><img src="/storage/cover_image/baseline_analytics_white_18dp.png" class="center" alt="ranking">
-                @if (Route::currentRouteName() === 'ranking.index')
-                    <span>Ranking</span>
-                @endif
-            </a>
+        <div class="bar-navigation surface">
+            <div class="bar-navigation item">
+                <div class="bar-navigation item one">
+                    <a class="{{(Route::currentRouteName() === 'resources.index') ? 'bar-navigation enabledicon' : ''}}" href="/resources"><img src="/storage/cover_image/baseline_open_in_new_white_18dp.png" class="bar-navigation disabledicon" alt="resources"></a>
+                    @if (Route::currentRouteName() === 'resources.index')
+                        <span class="bar-navigation enabledlabel">Resources</span>
+                    @endif
+                </div>
+                <div class="bar-navigation item two">
+                    <a class="{{(Route::currentRouteName() === 'account.index') ? 'bar-navigation enabledicon' : ''}}" href="/account"><img src="/storage/cover_image/baseline_account_circle_white_18dp.png" class="bar-navigation disabledicon" alt="account"></a>
+                    @if (Route::currentRouteName() === 'account.index')
+                            <span class="bar-navigation enabledlabel">Account</span>
+                    @endif 
+                </div>
+                <div class="bar-navigation item three">
+                    <a class="{{(Route::currentRouteName() === 'activities.index') ? 'bar-navigation enabledicon' : ''}}" href="/activities"><img src="/storage/cover_image/baseline_videogame_asset_white_18dp.png" class="bar-navigation disabledicon" alt="activities"></a>
+                    @if (Route::currentRouteName() === 'activities.index')
+                        <span class="bar-navigation enabledlabel">Activities</span>
+                    @endif
+                </div>
+                <div class="bar-navigation item four">
+                    <a class="{{(Route::currentRouteName() === 'social.index') ? 'bar-navigation enabledicon2' : ''}}" href="/social"><img src="/storage/cover_image/baseline_supervisor_account_white_18dp.png" class="bar-navigation disabledicon" alt="social"></a>
+                    @if (Route::currentRouteName() === 'social.index')
+                        <span class="bar-navigation enabledlabel">Social</span>
+                    @endif
+                </div>
+                <div class="bar-navigation item five">
+                    <a class="{{(Route::currentRouteName() === 'ranking.index') ? 'bar-navigation enabledicon2' : ''}}" href="/ranking"><img src="/storage/cover_image/baseline_analytics_white_18dp.png" class="bar-navigation disabledicon" alt="ranking"></a> 
+                    @if (Route::currentRouteName() === 'ranking.index')
+                        <span class="bar-navigation enabledlabel">Ranking</span>
+                    @endif
+                </div>
+            </div>
         </div>
     </div>
 </nav>
