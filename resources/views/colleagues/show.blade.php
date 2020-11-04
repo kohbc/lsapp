079a5fa07729a29eb1eb964f700381b6c86139b4
kohbc <63275782+kohbc@users.noreply.github.com>
2020-11-04 16:54:28 +1100

    front-end@colleagues

--- resources/views/colleagues/show.blade.php
+++ resources/views/colleagues/show.blade.php
@@ -2,8 +2,8 @@
 
 @section('content')
     @if($colleague != null)
-        <h3>Name: {{$colleague->avatar_original}}</h3>
-        <h3>Name: {{$colleague->name}}</h3>
+        <img src="{{$colleague->avatar_original}}" class="list item image-list image">
+        <p class="list item label">{{$colleague->name}}</p>
         <small>Became colleague on {{$colleague->created_at}}</small><br/>
     @else
         <p>No such colleague exist.</p>
